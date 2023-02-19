03cac0b518e67363483a1711c9a00ea4b33e12e2
Daniel Vaca Araujo <vacaaraujo@google.com>
2023-02-19 19:19:53 +0100

    fix: themes href

--- src/prebuiltThemes.vue
+++ src/prebuiltThemes.vue
@@ -2,32 +2,32 @@
   <h2>Example themes</h2>
   <div class="hstack gap-3">
     <a
-      href="/?body-bg=ffffff&body-color=333333&border-color=e6e6e6&input-border-color=a6b1c9&primary=3a4eff&danger=ff1f4a&warning=fc9929&success=22b573&disabled-color=c7ccd6&secondary-bg=f9fafb&secondary-color=363c49&tertiary-bg=eceef4&tertiary-color=626c84&primary-bg=cfddfc&primary-color=062b79&danger-bg=fae1e0&danger-color=6d1412&warning-bg=fff2e5&warning-color=804000&success-bg=ecf9ee&success-color=20602a&theme-name=Default+light&theme-type=light"
+      href="?body-bg=ffffff&body-color=333333&border-color=e6e6e6&input-border-color=a6b1c9&primary=3a4eff&danger=ff1f4a&warning=fc9929&success=22b573&disabled-color=c7ccd6&secondary-bg=f9fafb&secondary-color=363c49&tertiary-bg=eceef4&tertiary-color=626c84&primary-bg=cfddfc&primary-color=062b79&danger-bg=fae1e0&danger-color=6d1412&warning-bg=fff2e5&warning-color=804000&success-bg=ecf9ee&success-color=20602a&theme-name=Default+light&theme-type=light"
     >
       Default
     </a>
     <a
-      href="/?body-bg=272e3f&body-color=ffffff&border-color=495674&input-border-color=58698d&primary=86aaf9&danger=ff667f&warning=f5993d&success=00e5b9&disabled-color=576175&secondary-bg=313a4e&secondary-color=d3d7de&tertiary-bg=414d68&tertiary-color=a7aebe&primary-bg=3e4f74&primary-color=cfddfc&danger-bg=4d3337&danger-color=ffccd4&warning-bg=43403d&warning-color=facc9e&success-bg=21453e&success-color=4cffdd&theme-name=Default+dark&theme-type=dark"
+      href="?body-bg=272e3f&body-color=ffffff&border-color=495674&input-border-color=58698d&primary=86aaf9&danger=ff667f&warning=f5993d&success=00e5b9&disabled-color=576175&secondary-bg=313a4e&secondary-color=d3d7de&tertiary-bg=414d68&tertiary-color=a7aebe&primary-bg=3e4f74&primary-color=cfddfc&danger-bg=4d3337&danger-color=ffccd4&warning-bg=43403d&warning-color=facc9e&success-bg=21453e&success-color=4cffdd&theme-name=Default+dark&theme-type=dark"
     >
       Default dark
     </a>
     <a
-      href="/?body-bg=ffffff&body-color=000000&border-color=000000&input-border-color=000000&primary=5257ff&danger=ff0000&warning=ffa500&success=32cd32&disabled-color=808080&secondary-bg=f5f5f5&secondary-color=000000&tertiary-bg=dcdcdc&tertiary-color=000000&primary-bg=b3bfff&primary-color=3d3f8a&danger-bg=f7a1a1&danger-color=ff0000&warning-bg=ffe1a8&warning-color=ffa500&success-bg=bbf7bb&success-color=32cd32&theme-name=High+contrast+light&theme-type=light"
+      href="?body-bg=ffffff&body-color=000000&border-color=000000&input-border-color=000000&primary=5257ff&danger=ff0000&warning=ffa500&success=32cd32&disabled-color=808080&secondary-bg=f5f5f5&secondary-color=000000&tertiary-bg=dcdcdc&tertiary-color=000000&primary-bg=b3bfff&primary-color=3d3f8a&danger-bg=f7a1a1&danger-color=ff0000&warning-bg=ffe1a8&warning-color=ffa500&success-bg=bbf7bb&success-color=32cd32&theme-name=High+contrast+light&theme-type=light"
     >
       High contrast light
     </a>
     <a
-      href="/?body-bg=000000&body-color=ffffff&border-color=ffffff&input-border-color=ffffff&primary=86aaf9&danger=ff667f&warning=f5993d&success=00e5b9&disabled-color=576175&secondary-bg=313a4e&secondary-color=d3d7de&tertiary-bg=414d68&tertiary-color=a7aebe&primary-bg=3e4f74&primary-color=cfddfc&danger-bg=4d3337&danger-color=ffccd4&warning-bg=43403d&warning-color=facc9e&success-bg=21453e&success-color=4cffdd&theme-name=High+contrast+dark&theme-type=dark"
+      href="?body-bg=000000&body-color=ffffff&border-color=ffffff&input-border-color=ffffff&primary=86aaf9&danger=ff667f&warning=f5993d&success=00e5b9&disabled-color=576175&secondary-bg=313a4e&secondary-color=d3d7de&tertiary-bg=414d68&tertiary-color=a7aebe&primary-bg=3e4f74&primary-color=cfddfc&danger-bg=4d3337&danger-color=ffccd4&warning-bg=43403d&warning-color=facc9e&success-bg=21453e&success-color=4cffdd&theme-name=High+contrast+dark&theme-type=dark"
     >
       High contrast dark
     </a>
     <a
-      href="/?body-bg=1a1a1a&body-color=ebebeb&border-color=333333&input-border-color=666666&primary=5a5aff&danger=ff5a5a&warning=ffd966&success=4cd964&disabled-color=808080&secondary-bg=333333&secondary-color=ebebeb&tertiary-bg=4d4d4d&tertiary-color=ebebeb&primary-bg=5a5aff&primary-color=ebebeb&danger-bg=ff5a5a&danger-color=ebebeb&warning-bg=ffd966&warning-color=1a1a1a&success-bg=4cd964&success-color=ebebeb&theme-name=Custom+dark&theme-type=dark"
+      href="?body-bg=1a1a1a&body-color=ebebeb&border-color=333333&input-border-color=666666&primary=5a5aff&danger=ff5a5a&warning=ffd966&success=4cd964&disabled-color=808080&secondary-bg=333333&secondary-color=ebebeb&tertiary-bg=4d4d4d&tertiary-color=ebebeb&primary-bg=5a5aff&primary-color=ebebeb&danger-bg=ff5a5a&danger-color=ebebeb&warning-bg=ffd966&warning-color=1a1a1a&success-bg=4cd964&success-color=ebebeb&theme-name=Custom+dark&theme-type=dark"
     >
       Custom dark
     </a>
     <a
-      href="/?body-bg=000000&body-color=00FF00&border-color=00FF00&input-border-color=00FF00&primary=00FF00&danger=FF0000&warning=FFFF00&success=00FF00&disabled-color=808080&secondary-bg=101010&secondary-color=00FF00&tertiary-bg=202020&tertiary-color=00FF00&primary-bg=000000&primary-color=00FF00&danger-bg=000000&danger-color=FF0000&warning-bg=000000&warning-color=FFFF00&success-bg=000000&success-color=00FF00&theme-name=Matrix&theme-type=dark"
+      href="?body-bg=000000&body-color=00FF00&border-color=00FF00&input-border-color=00FF00&primary=00FF00&danger=FF0000&warning=FFFF00&success=00FF00&disabled-color=808080&secondary-bg=101010&secondary-color=00FF00&tertiary-bg=202020&tertiary-color=00FF00&primary-bg=000000&primary-color=00FF00&danger-bg=000000&danger-color=FF0000&warning-bg=000000&warning-color=FFFF00&success-bg=000000&success-color=00FF00&theme-name=Matrix&theme-type=dark"
     >
       Matrix
     </a>
